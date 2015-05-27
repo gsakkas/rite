e1814858f5a4fca757b04bdbfe7a1674560dfada
Eric Seidel <gridaphobe@gmail.com>
2015-05-26 17:55:10 -0700

    more parsing

--- src/NanoML.hs
+++ src/NanoML.hs
@@ -41,12 +41,14 @@ data Literal
   | LU
   deriving (Show)
 
+data RecFlag = Rec | NonRec deriving (Show)
+
 data Expr
   = Var Var
   | Lam Var Expr
   | App Expr Expr
   | Lit Literal
-  | Let Var Expr Expr
+  | Let RecFlag Pat Expr Expr
   | Ite Expr Expr Expr
   | Seq Expr Expr -- TODO: do we actually need this for the student examples?
   | Case Expr [Alt]
@@ -59,6 +61,8 @@ data Pat
   | LitPat Literal
   | ListPat [Pat]
   | TuplePat [Pat]
+  | FunPat Var [Var] -- TODO: should this be `[Pat]`?
+  | WildPat
   deriving (Show)
 
 
@@ -97,8 +101,33 @@ expr =   buildExpressionParser table compound
      <?> "expression"
 
 compound :: Parser Expr
-compound =   ite <|> app
+compound =   let_ <|> ite <|> app
          <?> "compound expression"
+
+let_ :: Parser Expr
+let_ = do reserved "let"
+          b <- Rec <$ reserved "rec" <|> return NonRec
+          p <- pat
+          reservedOp "="
+          e <- expr
+          reserved "in"
+          body <- expr
+          return (Let b p e body)
+
+pat :: Parser Pat
+pat =   WildPat <$ reserved "_"
+    <|> LitPat <$> literal
+    <|> ListPat <$> listOf pat
+    <|> TuplePat <$> tupleOf pat
+    <|> funVarPat
+    <?> "pattern"
+
+funVarPat :: Parser Pat
+funVarPat = do v  <- identifier
+               vs <- many identifier
+               if null vs
+                 then return (VarPat v)
+                 else return (FunPat v vs)
 
 ite :: Parser Expr
 ite = do reserved "if"
@@ -118,8 +147,11 @@ app = do f    <- term
 term :: Parser Expr
 term =   parens expr
      <|> Lit <$> literal
-     <|> Var <$> identifier
+     <|> Var <$> var
      <?> "simple expression"
+
+var :: Parser Var
+var = identifier
 
 literal :: Parser Literal
 literal =   either (LI . fromInteger) LD <$> try integerOrDouble
@@ -127,8 +159,8 @@ literal =   either (LI . fromInteger) LD <$> try integerOrDouble
         <?> "literal"
 
 bool :: Parser Bool
-bool =   reserved "true"  *> pure True
-     <|> reserved "false" *> pure False
+bool =   True  <$ reserved "true"
+     <|> False <$ reserved "false"
      <?> "boolean"
 
 table :: [[Operator Parser Expr]]
@@ -141,8 +173,15 @@ table  = [ [ binary "*"  AssocLeft, binary "/"  AssocLeft
          ]
 
 binary :: String -> Assoc -> Operator Parser Expr
-binary name assoc = Infix (mkOp <* reservedOp name) assoc
+binary name = Infix (mkOp <* reservedOp name)
   where
   mkOp = return $ \e1 e2 -> (App (App (Var name) e1) e2)
 -- prefix  name fun       = Prefix (fun <* reservedOp name)
 -- postfix name fun       = Postfix (fun <* reservedOp name)
+
+listOf :: Parser a -> Parser [a]
+listOf p = brackets (p `sepBy` semi)
+
+tupleOf :: Parser a -> Parser [a]
+tupleOf p = parens (p `sepBy` comma)
+
