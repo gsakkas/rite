4ca824d47458631d6d7fba0a08e920cc48cb4646
Eric Seidel <gridaphobe@gmail.com>
2015-10-28 17:11:58 -0700

    ugh

--- src/NanoML.hs
+++ src/NanoML.hs
@@ -145,7 +145,7 @@ checkDecl f prog = do
                     (\r -> Hole Nothing r Nothing) <$> fresh
           fo (Var Nothing f) args
 
-        go (f,st,v) r ((m+1) `mod` 20)
+        go (f,st,v) r ((m+1) `mod` (maxSteps stdOpts))
 
 
   unifyNoExn t1 t2 = unify (unExn t1) (unExn t2)
