82efeca9f8859ae076014bff7be33c9b5f498e30
Eric Seidel <gridaphobe@gmail.com>
2015-11-06 19:46:17 -0800

    regen json blobs

--- survey/data/prog0223.ml
+++ survey/data/prog0223.ml
@@ -4,4 +4,4 @@ let bigMul l1 l2 =
   let base = (0, []) in
   let args = l1 in let (_,res) = List.fold_left f base args in res;;
 
-let _ = bigMul [9; 9; 9; 9] [9; 9; 9; 9];;
+let _ = bigMul [9] [9];;
