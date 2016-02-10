7f65f2305d4f67235f31040d1d12a05fb299c7a1
Eric Seidel <gridaphobe@gmail.com>
2016-02-10 13:48:25 -0800

    basic tour

--- survey/demo.js
+++ survey/demo.js
@@ -520,6 +520,27 @@ function setup() {
 
   editor.setValue(demos.factorial);
   draw(factrace);
+
+  // Instance the tour
+  var tour = new Tour({
+    steps: [
+    {
+      element: "#my-element",
+      title: "Title of my step",
+      content: "Content of my step"
+    },
+    {
+      element: "#my-other-element",
+      title: "Title of my step",
+      content: "Content of my step"
+    }
+    ]});
+
+  // Initialize the tour
+  tour.init();
+
+  // Start the tour
+  tour.start();
 }
 
 function draw(data) {
