282584e03241409bf270b3ef44fad45899584ce2
Eric Seidel <gridaphobe@gmail.com>
2016-02-15 18:24:07 -0800

    fix redex annotation..

--- survey/data/prog0971.ml
+++ survey/data/prog0971.ml
@@ -1,6 +1,7 @@
 (* `pipe [f1;...;fn]` (where f1,...,fn are functions!)
    returns a function `f` such that for any `x`, we have
    `f x` returns `fn(...(f2(f1 x)))` *)
+
 let pipe fs = 
   let f acc el = el acc in
   let base x = x in
