00ddbf3594a70f6db724b946b9863c35a0b4780f
Eric Seidel <gridaphobe@gmail.com>
2016-10-12 14:45:00 -0700

    uygh

--- bin/Generate.hs
+++ bin/Generate.hs
@@ -5,6 +5,7 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as LBSC
 import Data.Either
 import qualified Data.Map.Strict as Map
+import Data.Maybe
 import GHC.Generics
 
 import NanoML.Classify
@@ -13,27 +14,36 @@ import NanoML.Pretty
 import NanoML.Types
 
 
+import Debug.Trace
+
 main :: IO ()
 main = interact (unlines
                  -- TODO: add runtime flag to choose classifier?
-                 . map (generateDiff by_ops)
+                 . mapMaybe (generateDiff by_ops)
                  . lines)
 
 generateDiff :: (ToJSON a, Monoid a)
-             => (Expr -> a) -> String -> String
+             => (Expr -> a) -> String -> Maybe String
 generateDiff mkfs json = case eitherDecode (LBSC.pack json) of
-  Left e -> error e
-  -- Right (MkInSample bads' [fix'])
-  Right (MkInSample bads' (fix':_))
+  Left e -> {-trace e-} Nothing
+  Right (MkInSample bads' [fix'])
+  --Right (MkInSample bads' (fix':_))
     | Right fix <- parseTopForm fix'
     , bads <- rights $ map parseTopForm bads'
-    -> unlines . map (LBSC.unpack . encode . diffOne mkfs fix) $ bads
+    -> Just . unlines . map (LBSC.unpack . encode . diffOne mkfs fix) $ bads
+  Right (MkInSample bads' [fix'])
+  --Right (MkInSample bads' (fix':_))
+    | Left e <- parseTopForm fix'
+    -> {-trace e-} Nothing
+  v -> error (show v)
 
 diffOne :: Monoid a => (Expr -> a) -> Prog -> Prog -> OutSample a
 diffOne mkfs fix bad = MkOutSample
   { prog = render (prettyProg bad)
   , ast  = bad
-  , loc  = diffProg bad fix
+  , loc  = case diffProg bad fix of
+             Just s -> Just s
+             Nothing -> error $ "\n" ++ (render (prettyProg bad)) ++ "\n\n" ++ (render (prettyProg fix))
   , features = mconcat (map mkfsD bad)
   }
   where
