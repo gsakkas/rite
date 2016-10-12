52ad13ffa9585f9ffc6c54cb576ac96886018a57
Eric Seidel <eric@seidel.io>
2016-10-12 14:43:22 -0700

    make classify take list of preds

--- bin/Generate.hs
+++ bin/Generate.hs
@@ -10,17 +10,21 @@ import GHC.Generics
 import NanoML.Classify
 import NanoML.Parser
 import NanoML.Pretty
-import NanoML.Types
+import NanoML.Types hiding (Kind)
 
 
 main :: IO ()
 main = interact (unlines
                  -- TODO: add runtime flag to choose classifier?
-                 . map (generateDiff by_ops)
+                 . map (generateDiff preds)
                  . lines)
 
-generateDiff :: (ToJSON a, Monoid a)
-             => (Expr -> a) -> String -> String
+preds :: [Expr -> Bool]
+preds = [has_op o | o <- [Eq .. FExp]]
+     ++ [has_con "::", has_con "[]"]
+
+
+generateDiff :: [Expr -> Bool] -> String -> String
 generateDiff mkfs json = case eitherDecode (LBSC.pack json) of
   Left e -> error e
   Right (MkInSample bads' [fix'])
@@ -28,26 +32,38 @@ generateDiff mkfs json = case eitherDecode (LBSC.pack json) of
     , bads <- rights $ map parseTopForm bads'
     -> unlines . map (LBSC.unpack . encode . diffOne mkfs fix) $ bads
 
-diffOne :: Monoid a => (Expr -> a) -> Prog -> Prog -> OutSample a
-diffOne mkfs fix bad = MkOutSample
-  { prog = render (prettyProg bad)
-  , ast  = bad
-  , loc  = diffProg bad fix
-  , features = mconcat (map mkfsD bad)
-  }
+diffOne :: [Expr -> Bool] -> Prog -> Prog -> [OutSample]
+-- diffOne mkfs fix bad = MkOutSample
+--   { prog = render (prettyProg bad)
+--   , ast  = bad
+--   , loc  = diffProg bad fix
+--   , features = mconcat (map mkfsD bad)
+--   }
+--   where
+diffOne fs fix bad = case diffProg bad fix of
+  Nothing -> []
+  Just l  -> map (mkOut l) (concatMap mkfsD bad)
   where
-  mkfsD (DFun _ _ pes) = mconcat (map (mkfs.snd) pes)
-  mkfsD (DEvl _ e) = mkfs e
+  mkfsD (DFun _ _ pes) = mconcat (map (classify fs.snd) pes)
+  mkfsD (DEvl _ e) = classify fs e
   mkfsD _ = mempty
+
+mkOut :: SrcSpan -> (SrcSpan, [Bool]) -> OutSample
+mkOut l1 (l2, fs) = MkOutSample (if l1 == l2 then Bad else Good) fs
 
 data InSample = MkInSample { bad :: [String], fix :: [String] }
   deriving (Show, Generic)
 instance FromJSON InSample
 
-data OutSample a = MkOutSample
-  { prog :: String
-  , ast :: Prog
-  , loc :: MSrcSpan
-  , features :: a
+data OutSample = MkOutSample
+  -- { prog :: String
+  -- , ast :: Prog
+  -- , loc :: MSrcSpan
+  { kind :: Kind
+  , features :: [Bool]
   } deriving (Show, Generic)
-instance ToJSON a => ToJSON (OutSample a)
+instance ToJSON OutSample
+
+data Kind = Good | Bad
+  deriving (Show, Generic)
+instance ToJSON Kind
