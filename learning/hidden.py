separate output regularizer, fix l2 computation

learning/hidden.py
@@ -9,7 +9,7 @@ import math
 import util
 
 def build_model(features, labels, hidden,
-                learn_rate=0.1, beta=0.01, model_dir=None):
+                learn_rate=0.1, beta=0.01, beta_out=0.01, model_dir=None):
     '''Build a linear classifier with fully-connected hidden layers.
 
     @param features: A list of feature names.
@@ -22,7 +22,7 @@ def build_model(features, labels, hidden,
     @return: A 4-tuple of training, testing, plotting, and closing functions.
     '''
 
-    print 'building MLP with alpha=%f, beta=%f' % (learn_rate, beta)
+    #print 'building MLP with alpha=%f, beta=%f' % (learn_rate, beta)
 
     n_in = len(features)
     n_out = len(labels)
@@ -66,8 +66,8 @@ def build_model(features, labels, hidden,
                         name='b')
         util.variable_summaries(W,'W')
         util.variable_summaries(b,'b')
-        Ws.append(W)
-        bs.append(b)
+        # Ws.append(W)
+        # bs.append(b)
         y = tf.matmul(h, W) + b
 
     y_ = tf.placeholder(tf.float32, [None, n_out], name='y_')
@@ -76,11 +76,12 @@ def build_model(features, labels, hidden,
 
     with tf.name_scope('cross_entropy'):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_)
-        regularizers = beta * sum(tf.nn.l2_loss(W) for W in Ws) # + sum(tf.nn.l2_loss(b) for b in bs)
-        tf.summary.scalar('l2_loss', regularizers)
-        cross_entropy += regularizers
         loss = tf.reduce_mean(cross_entropy)
         tf.summary.scalar('loss', loss)
+        regularizers = beta * sum(tf.nn.l2_loss(W) for W in Ws)
+        regularizers += beta_out * tf.nn.l2_loss(W)
+        tf.summary.scalar('l2_loss', regularizers)
+        loss += regularizers
     with tf.name_scope('train'):
         # global_step = tf.Variable(0, trainable=False)
         # learning_rate = tf.train.exponential_decay(learn_rate, global_step,
@@ -95,7 +96,7 @@ def build_model(features, labels, hidden,
 
     sess = tf.InteractiveSession()
     merged = tf.summary.merge_all()
-    summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
+    #summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
 
     if n_out >= 2:
         correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(y),1), tf.argmax(y_,1))
@@ -124,8 +125,8 @@ def build_model(features, labels, hidden,
                                   feed_dict={x: data[features], y_: data[labels],
                                              # keep_prob: 0.5})
                                              keep_prob: 1.0})  # TODO: should we use dropout??
-        if i % 100 == 0:
-            summary_writer.add_summary(summary_str, i)
+        #if i % 100 == 0:
+        #    summary_writer.add_summary(summary_str, i)
         if validation is not None and i % 100 == 0:
             acc = 0
             acc1 = 0
@@ -262,7 +263,7 @@ def build_model(features, labels, hidden,
             print('avg / std / med samples: %.2f / %.2f / %.2f' % (np.mean(ts), np.std(ts), np.median(ts)) )
             print('avg / std / med changes: %.2f / %.2f / %.2f' % (np.mean(cs), np.std(cs), np.median(cs)) )
 
-            saver.save(sess, 'hidden_model')
+            #saver.save(sess, 'hidden_model')
 
         return {'top-1': acc1, 'top-2': acc2, 'top-3': acc3, 'recall': np.mean(rs)}
 

learning/learn.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+
 import math
 import os.path
 import random
+import sys
 
 from hyperopt import fmin, tpe, hp
 import numpy as np
@@ -30,6 +33,7 @@ flags.DEFINE_integer("n_batches", None, "Number of training rounds.")
 flags.DEFINE_integer("n_epochs", None, "Number of training rounds.")
 flags.DEFINE_float("learn_rate", 0.1, "Learning rate.")
 flags.DEFINE_float("reg_rate", 0.01, "Regularization rate.")
+flags.DEFINE_float("reg_out_rate", 0.01, "Regularization rate for output.")
 flags.DEFINE_integer("n_folds", 1, "Number of folds for cross-validation.")
 flags.DEFINE_bool("plot", False, "Plot the learned model.")
 flags.DEFINE_bool("only_slice", False, "Only look at exprs in the error slice.")
@@ -47,44 +51,48 @@ def load_data(path):
         f = os.path.join(path, csv)
         df, fs, ls = input.load_csv(f, only_slice=FLAGS.only_slice)
         if df is None:
-            print i
+            print(i)
             continue
         if df.shape[0] == 0:
-            print i
+            print(i)
             continue
         dfs.append((f, df))
     return (dfs, fs, ls)
 
 def cross_validate(dfs, fs, ls):
-    kf = KFold(n_splits=FLAGS.n_folds, random_state=FLAGS.seed)
-    #kf = ShuffleSplit(n_splits=1, test_size=0.25, random_state=0)
+    #kf = KFold(n_splits=FLAGS.n_folds, random_state=FLAGS.seed)
+    kf = ShuffleSplit(n_splits=1, test_size=0.25, random_state=FLAGS.seed)
     # print(len(dfs))
     rs = []
     for i, (train, test) in enumerate(kf.split(dfs)):
+        print('.', end='')
+        sys.stdout.flush()
         # print(len(train), len(test))
         rs.append(train_and_eval(dfs, fs, ls, i=i, train_idxs=train, test_idxs=test))
     return {'top-1': np.mean([r['top-1'] for r in rs]),
             'top-2': np.mean([r['top-2'] for r in rs]),
             'top-3': np.mean([r['top-3'] for r in rs])}
 
-def objective(dfs, fs, ls, a, b, m):
-    print 'learn_rate', a, 'reg_rate', b, 'mini_batch', m
+def objective(dfs, fs, ls, a, b, bo):
+    #print('learn_rate', 10**a, 'reg_rate', 10**b, 'reg_out', 10**bo, 'mini_batch', m)
+    print('learn_rate', 10**a, 'reg_rate', 10**b, 'reg_out', 10**bo)
     FLAGS.learn_rate = 10 ** a
     FLAGS.reg_rate = 10 ** b
-    FLAGS.batch_size = int(m)
+    FLAGS.reg_out_rate = 10 ** bo
+    #FLAGS.batch_size = int(m)
     loss = 1 - cross_validate(dfs, fs, ls)['top-1']
-    print loss
+    print(loss)
     return loss
 
 def hyperopt(dfs, fs, ls):
     space = (hp.uniform('alpha', -4, -1),
-             hp.uniform('beta',  -3, 0),
-             hp.quniform('mini', 100, 500, 10))
-    best = fmin(fn=lambda (a, b, m): objective(dfs, fs, ls, a, b, m),
+             hp.uniform('beta',  -2, 0),
+             hp.uniform('beta_out',  -3, 0))
+    best = fmin(fn=lambda (a, b, bo): objective(dfs, fs, ls, a, b, bo),
                 space=space,
                 algo=tpe.suggest,
-                max_evals=500)
-    print best
+                max_evals=100)
+    print(best)
 
 def main(_):
     random.seed(FLAGS.seed)
@@ -174,6 +182,7 @@ def build_model(fs, ls, model_dir):
         return hidden.build_model(fs, ls, hidden_layers,
                                   learn_rate=FLAGS.learn_rate,
                                   beta=FLAGS.reg_rate,
+                                  beta_out=FLAGS.reg_out_rate,
                                   model_dir=model_dir)
     else:
         raise ("unknown model type: " + FLAGS.model)

learning/linear.py
@@ -37,11 +37,11 @@ def build_model(features, labels, learn_rate=0.1, beta=0.01, model_dir=None):
 
     with tf.name_scope('cross_entropy'):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_),
-        regularizers = tf.nn.l2_loss(W) # + tf.nn.l2_loss(b)
-        tf.summary.scalar('l2_loss', regularizers)
-        cross_entropy += beta * regularizers
         loss = tf.reduce_mean(cross_entropy)
         tf.summary.scalar('loss', loss)
+        regularizers = tf.nn.l2_loss(W) # + tf.nn.l2_loss(b)
+        tf.summary.scalar('l2_loss', regularizers)
+        loss += beta * regularizers
     with tf.name_scope('train'):
         # global_step = tf.Variable(0, trainable=False)
         # learning_rate = tf.train.exponential_decay(learn_rate, global_step,
@@ -56,7 +56,7 @@ def build_model(features, labels, learn_rate=0.1, beta=0.01, model_dir=None):
 
     sess = tf.InteractiveSession()
     merged = tf.summary.merge_all()
-    summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
+    #summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
 
     if n_out >= 2:
         correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(y),1), tf.argmax(y_,1))
@@ -80,8 +80,8 @@ def build_model(features, labels, learn_rate=0.1, beta=0.01, model_dir=None):
     def train(data, i, validation=None, verbose=False):
         summary_str, _ = sess.run([merged, train_step],
                                   feed_dict={x: data[features], y_: data[labels]})
-        if i % 100 == 0:
-            summary_writer.add_summary(summary_str, i)
+        #if i % 100 == 0:
+        #    summary_writer.add_summary(summary_str, i)
         if validation is not None:
             # acc = sess.run(accuracy,
             #                feed_dict={x: validation[features], y_: validation[labels]})