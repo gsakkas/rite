338bd5858ae0c7ae2bd03a41f9ee5f8a84f74960
George Sakkas <george.p.sakkas@gmail.com>
2020-10-21 11:46:26 -0700

    Added evaluation for LSTMs (NOT WORKING FULLY)

--- learning/predict_fixes_lstm.py
+++ learning/predict_fixes_lstm.py
@@ -102,7 +102,7 @@ max_target_len = max(map(lambda s: s.shape[0], train_targets))
 print("Input data:", (len(train_samples), max_input_len, n_input))
 print("Target data:", (len(train_targets), max_target_len, n_output))
 
-if method == 'lstm':
+if method == 'lstm' or method == 'load':
     # Define number of LSTM cells
     n_units = 256
 
@@ -140,17 +140,17 @@ else:
     sys.exit(0)
 
 
-# Generate target, given source sequence
-def predict_sequence(infenc, infdec, source, n_steps, cardinality):
+# Generate target, given an encoded source sequence
+def predict_sequence(encd_model, decd_model, source, n_steps, cardl):
     # Encode
-    state = infenc.predict(source)
+    state = encd_model.predict(source)
     # Start of sequence input
-    target_seq = np.zeros((cardinality, )).reshape(1, 1, cardinality)
+    target_seq = np.zeros((1, 1, cardl))
     # Collect predictions
     output = list()
-    for t in range(n_steps):
+    for _ in range(n_steps):
         # Predict next node
-        yhat, h, c = infdec.predict([target_seq] + state)
+        yhat, h, c = decd_model.predict([target_seq] + state)
         # Store prediction
         output.append(yhat[0,0,:])
         # Update state
@@ -160,10 +160,30 @@ def predict_sequence(infenc, infdec, source, n_steps, cardinality):
     return np.array(output)
 
 
-def shift_targets(trgt):
-    new_trgt = trgt.to_numpy()[:-1,:]
-    start    = np.zeros((1, trgt.shape[1]), dtype=int)
+def predict_next_node(encd_model, decd_model, source, known_nodes, cardl):
+    # Encode
+    state = encd_model.predict(source)
+    # Collect predictions
+    output = list()
+    for k_node in known_nodes:
+        # Predict next node
+        yhat, h, c = decd_model.predict([k_node.reshape(1, 1, cardl)] + state)
+        # Store prediction
+        output.append(yhat[0,0,:])
+        # Update state
+        state = [h, c]
+    return output[-1]
+
+
+def one_hot_decode(encoded_seq):
+    return [np.argmax(vector) for vector in encoded_seq]
+
+
+def shift_targets(target):
+    new_trgt = target.to_numpy()[:-1,:]
+    start    = np.zeros((1, target.shape[1]), dtype=int)
     return np.concatenate((start, new_trgt), axis=0)
+
 
 def pad_to_length(arr, max_len):
     rs = arr.shape[0]
@@ -190,234 +210,24 @@ if method == 'lstm':
 elif method == 'load':
     model.load_weights(join(train_dir, 'models', 'lstm-256-full-progs.h5'))
 
-sys.exit(0) ### FIXME: Works until here
 
-
-prob_error = []
-anses = []
-loc_conf = []
-if model == 'popular':
-    prob_error = [[0, 1, 2, 3, 4, 5, 6] for _ in range(test_stds_all.shape[0])]
-    anses = [1 for _ in range(test_stds_all.shape[0])]
-    loc_conf = [np.random.rand() * 100 for _ in range(test_stds_all.shape[0])]
-elif model == 'random':
-    prob_error = [np.random.permutation(num_of_cls) for _ in range(test_stds_all.shape[0])]
-    anses = [x[0] + 1 for x in prob_error]
-    loc_conf = [np.random.rand() * 100 for _ in range(test_stds_all.shape[0])]
-elif model == 'svm' or model == 'trees':
-    anses = clf.predict(test_stds_all).tolist()
-    prob_score = clf.predict_proba(test_stds_all)
-    prob_error = [np.argsort(pe)[::-1].tolist() for pe in prob_score]
-    loc_conf = [np.random.rand() * 100 for _ in range(test_stds_all.shape[0])]
-else:
-    prob_score = clf.predict(test_stds_all)
-    anses = [np.argmax(pe) + 1 for pe in prob_score]
-    # print(len(prob_score))
-    # print(len(prob_score[0]))
-    prob_error = [np.argsort(pe)[::-1].tolist() for pe in prob_score]
-    # print(len(prob_error))
-    # print(len(prob_error[0]))
-
-pes_top6 = [[x + 1 for x in pe[:6]] for pe in prob_error]
-pes1, pes2, pes3, pes4, pes5, pes6 = map(list, zip(*pes_top6))
-all_labels = categorize(test_data.loc[:, 'L-Cluster1':last_L])
-ll = list(zip(test_span, loc_conf, pes1, pes2, pes3, pes4, pes5, pes6, all_labels.values))
-
-score = pd.DataFrame(data=ll, index=all_labels.index, columns=['SourceSpan', 'Loc-Conf', 'P-1', 'P-2', 'P-3', 'P-4', 'P-5', 'P-6', 'Actual'])
-
-all_programs = 0
-c1 = 0
-c3 = 0
-c5 = 0
-c6 = 0
-c1_whole = 0
-c3_whole = 0
-c5_whole = 0
-c6_whole = 0
-all_locs = 0
-locs3 = 0
-locs5 = 0
-alocs3 = 0
-alocs5 = 0
-in_top5_locs = []
-
-for labelind in list(set(all_labels.index)):
-    all_programs += 1
-    temp_sc = score.loc[[labelind]]
-    yay1 = 0
-    yay3 = 0
-    yay5 = 0
-    yay6 = 0
-    total = 0
-    for sc in temp_sc.values:
-        act = sc[8]
-        if act > 0:
-            total += 1
-            if act in sc[2:8]:
-                yay6 += 1
-            if act in sc[2:7]:
-                yay5 += 1
-            if act in sc[2:5]:
-                yay3 += 1
-            if sc[2] == act:
-                yay1 += 1
-    if total > 0:
-        c1 += float(yay1) * 100 / total
-        c3 += float(yay3) * 100 / total
-        c5 += float(yay5) * 100 / total
-        c6 += float(yay6) * 100 / total
-
-        c1_whole += int(yay1 / total)
-        c3_whole += int(yay3 / total)
-        c5_whole += int(yay5 / total)
-        c6_whole += int(yay6 / total)
+correct   = 0.0
+all_progs = float(len(test_samples))
+for (smpl, trgt, lbls, f) in zip(test_samples, test_targets, test_labels, test_file):
+    sizes        = smpl.shape
+    shaped_smpl  = smpl.to_numpy()
+    first_change = np.argmax(lbls)
+    if first_change < trgt.shape[0]:
+        prediction   = predict_next_node(encoder_model,
+                                        decoder_model,
+                                        shaped_smpl.reshape(1, sizes[0], sizes[1]),
+                                        shaped_smpl[:first_change, :],
+                                        n_output)
+        pred_node   = np.argmax(prediction)
+        target_node = one_hot_decode(trgt.to_numpy())[first_change]
+        if pred_node == target_node:
+            correct += 1
     else:
-        c1 += 100.0
-        c3 += 100.0
-        c5 += 100.0
-        c6 += 100.0
+        print("Fixed program is smaller than buggy for:", f)
 
-        c1_whole += 1.0
-        c3_whole += 1.0
-        c5_whole += 1.0
-        c6_whole += 1.0
-
-    locs = temp_sc.loc[:, 'Loc-Conf']
-    locs = np.argsort(locs.values)[::-1].tolist()
-    labels = test_labels_all.loc[[labelind]]
-    all_locs += sum(labels.values)
-    prog_locs = sum(labels.values)
-    labels3 = labels.iloc[locs[:3]]
-    labels3 = sum(labels3.values)
-    locs3 += labels3 / prog_locs
-    labels5 = labels.iloc[locs[:5]]
-    labels5 = sum(labels5.values)
-    locs5 += labels5 / prog_locs
-    alocs3 += labels3
-    alocs5 += labels5
-    to_check = [0] * len(labels.values)
-    for i in locs[:5]:
-        to_check[i] = 1
-    in_top5_locs.extend(to_check)
-
-    if model != 'load' and model != 'load-cnn' and model != 'popular' and model != 'random':
-        filenm = test_file.loc[[labelind]].values[0].split('.')
-        res_dir = join(test_dir, model + '-' + str(num_of_cls) + '-' + clf_type)
-        if not exists(res_dir):
-            mkdir(res_dir)
-        temp_sc.to_csv(res_dir + '/' + filenm[0] + '.csv', index=False)
-
-assert len(in_top5_locs) == len(all_labels.values)
-
-yay1 = 0
-yay1_2 = 0
-yay3 = 0
-yay3_2 = 0
-yay5 = 0
-yay5_2 = 0
-yay6 = 0
-yay6_2 = 0
-tots = 0
-yays1 = [0] * num_of_cls
-yays3 = [0] * num_of_cls
-yays5 = [0] * num_of_cls
-yays6 = [0] * num_of_cls
-alls = [0] * num_of_cls
-for i, tmpl in enumerate(all_labels.values):
-    pes = pes_top6[i]
-    if tmpl > 0:
-        tots += 1
-        alls[tmpl - 1] += 1
-        if tmpl in pes[:6]:
-            if in_top5_locs[i]:
-                yay6_2 += 1
-            yay6 += 1
-            yays6[tmpl - 1] += 1
-        if tmpl in pes[:5]:
-            if in_top5_locs[i]:
-                yay5_2 += 1
-            yay5 += 1
-            yays5[tmpl - 1] += 1
-        if tmpl in pes[:3]:
-            if in_top5_locs[i]:
-                yay3_2 += 1
-            yay3 += 1
-            yays3[tmpl - 1] += 1
-        if pes[0] == tmpl:
-            if in_top5_locs[i]:
-                yay1_2 += 1
-            yay1 += 1
-            yays1[tmpl - 1] += 1
-
-# All template accuracies for top N template-classes
-# print("accuracy for top 6 per class (" + str(num_of_cls) + ")")
-# print("top 1")
-# print([float(x) * 100 / y if y != 0 else 0.0 for x, y in zip(yays1, alls)])
-# print("top 3")
-# print([float(x) * 100 / y if y != 0 else 0.0 for x, y in zip(yays3, alls)])
-# print("top 5")
-# print([float(x) * 100 / y if y != 0 else 0.0 for x, y in zip(yays5, alls)])
-# print("top 6")
-# print([float(x) * 100 / y if y != 0 else 0.0 for x, y in zip(yays6, alls)])
-
-# Total accuracy for all program locations together
-print("accuracy for top 6")
-print("top 1")
-print(float(yay1) * 100 / tots)
-print("top 3")
-print(float(yay3) * 100 / tots)
-print("top 5")
-print(float(yay5) * 100 / tots)
-print("top 6")
-print(float(yay6) * 100 / tots)
-
-# Total accuracy for all program locations together, with each template predictions counted
-# only if it was in the top 5 error localization predictions for that location
-# print("accuracy for top 6 per class (" + str(num_of_cls) + ") for best 5 locations")
-# print("top 1")
-# print(float(yay1_2) * 100 / tots)
-# print("top 3")
-# print(float(yay3_2) * 100 / tots)
-# print("top 5")
-# print(float(yay5_2) * 100 / tots)
-# print("top 6")
-# print(float(yay6_2) * 100 / tots)
-
-# Average of each program's accuracy of template prediction
-# Only for the locations that changed in the dataset (that have a correct template)
-# print("accuracy for top 6 (average template acc. per program)")
-# print("top 1")
-# print(float(c1) / all_programs)
-# print("top 3")
-# print(float(c3) / all_programs)
-# print("top 5")
-# print(float(c5) / all_programs)
-# print("top 6")
-# print(float(c6) / all_programs)
-
-# How many programs are predicted 100% correct for all their locations
-# print("accuracy for top 6 (acc. per 100% correct program)")
-# print("top 1")
-# print(float(c1_whole) * 100 / all_programs)
-# print("top 3")
-# print(float(c3_whole) * 100 / all_programs)
-# print("top 5")
-# print(float(c5_whole) * 100 / all_programs)
-# print("top 6")
-# print(float(c6_whole) * 100 / all_programs)
-
-# Error localization results
-print("accuracy for top 3 locations")
-# print("average acc. per program")
-# print(float(locs3) * 100 / all_programs)
-# print("acc for all locations")
-print(float(alocs3) * 100 / all_locs)
-
-print("accuracy for top 5 locations")
-# print("average acc. per program")
-# print(float(locs5) * 100 / all_programs)
-# print("acc for all locations")
-print(float(alocs5) * 100 / all_locs)
-
-# if pes1 != anses:
-#     print('NOT OK: Check anses/pes difference for possible error!')
+print("Accuracy for first changed term prediction:", correct * 100.0 / all_progs)
